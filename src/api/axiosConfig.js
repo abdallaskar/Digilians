c38cc723a1c559a4ec65d8685576d53a637a7a6e
Abdall Sakr <abdallaskar2000@gmail.com>
2025-09-27 17:49:16 +0300

    edit in base url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api", // change to your backend URL
+    // baseURL: "http://localhost:5000/api", // change to your backend URL
+    baseURL: "https://digilians-backend.vercel.app/api",
     headers: {
         "Content-Type": "application/json",
     },
